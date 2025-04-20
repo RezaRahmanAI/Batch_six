f09cead47307b25c566097424b1175c36952b817
mahfuz243 <mehedihasanmahfuz66@gmail.com>
2025-04-21 01:41:12 +0600

    Admincontrollers

---
+++ Batch_six/Controllers/AdminsController.cs
@@ -0,0 +1,54 @@
+﻿using Batch_six.Data;
+using Batch_six.Domain;
+using Batch_six.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Batch_six.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminsController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public AdminsController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+
+        //
+        [HttpPost]
+        public async Task<IActionResult> CreateAdmins(CreateAdminRequestDTO request)
+        {
+            //map DTO to Domain Model
+            var admin = new Admin
+
+            {
+                Id = request.Id,
+                Name = request.Name,
+                Email = request.Email,
+                Password = request.Password,
+
+
+            }; 
+
+            await dbContext.Admins.AddAsync(admin);
+            await dbContext.SaveChangesAsync();
+
+            // map model to DTO
+            var response = new AdminDto
+            {
+                Id = admin.Id,
+                Name = admin.Name,
+                Email = admin.Email,
+                Password = admin.Password,
+
+            };
+
+            return Ok(response);
+        }
+    }
+}
---
+++ Batch_six/DTO/CreateAdminRequestDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Batch_six.DTO
+{
+    public class CreateAdminRequestDTO
+    {
+        public required int Id { get; set; }
+
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+}
